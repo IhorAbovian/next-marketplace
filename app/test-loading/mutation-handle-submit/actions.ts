e384343313f15cb85773ffc8097fa18963a080c9
Igor Abovyan <abovyan1903@gmail.com>
2026-08-23 20:21:18 -0400

    refactor: extract homepage data fetching to lib/data.ts"

--- app/test-loading/mutation-handle-submit/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { addItem } from "@/app/test-loading/_lib/delay";
-
-export async function createItemAction(formData: FormData) {
-  const name = String(formData.get("name") ?? "");
-  const item = await addItem(name);
-  return { created: item.name };
-}
--- app/test-loading/mutation-transition/actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { addItem } from "@/app/test-loading/_lib/delay";
-
-export async function likeAction() {
-  const item = await addItem("Liked item");
-  return item.id;
-}
